8d880a59c37f9ce1ede362e3b76858fee8efac5a
lbhdc <monolithic@lbhdc>
2020-05-23 17:30:56 -0500

    refactored function parameter names

--- src/math/vector.h
+++ src/math/vector.h
@@ -20,58 +20,58 @@ namespace Math {
     }
   };
 
-  inline std::istream& operator>>(std::istream &is, Vector &p) {
-    is >> p.x >> p.y >> p.z;
+  inline std::istream& operator>>(std::istream &is, Vector &v) {
+    is >> v.x >> v.y >> v.z;
     return is;
   }
 
-  inline std::ostream& operator<<(std::ostream &os, const Vector &p) {
-    os << p.x << p.y << p.z;
+  inline std::ostream& operator<<(std::ostream &os, const Vector &v) {
+    os << v.x << v.y << v.z;
     return os;
   }
 
-  inline Vector operator+(const Vector &p1, const Vector &p2) {
-    return Vector{p1.x + p2.x, p1.y + p2.y, p1.z + p2.z};
+  inline Vector operator+(const Vector &a, const Vector &b) {
+    return Vector{a.x + b.x, a.y + b.y, a.z + b.z};
   }
 
-  inline Vector operator-(const Vector &p1, const Vector &p2) {
-    return Vector{p1.x - p2.x, p1.y - p2.y, p1.z - p2.z};
+  inline Vector operator-(const Vector &a, const Vector &b) {
+    return Vector{a.x - b.x, a.y - b.y, a.z - b.z};
   }
 
-  inline Vector operator*(const Vector &p1, const Vector &p2) {
-    return Vector{p1.x * p2.x, p1.y * p2.y, p1.z * p2.z};
+  inline Vector operator*(const Vector &a, const Vector &b) {
+    return Vector{a.x * b.x, a.y * b.y, a.z * b.z};
   }
 
-  inline Vector operator/(const Vector &p1, const Vector &p2) {
-    return Vector{p1.x / p2.x, p1.y / p2.y, p1.z / p2.z};
+  inline Vector operator/(const Vector &a, const Vector &b) {
+    return Vector{a.x / b.x, a.y / b.y, a.z / b.z};
   }
 
-  inline Vector operator*(const Vector &p, double s) {
-    return Vector{p.x * s, p.y * s, p.z * s};
+  inline Vector operator*(const Vector &vector, double scaler) {
+    return Vector{vector.x * scaler, vector.y * scaler, vector.z * scaler};
   }
 
-  inline Vector operator*(double s, const Vector &p) {
-    return Vector{p.x * s, p.y * s, p.z * s};
+  inline Vector operator*(double scaler, const Vector &vector) {
+    return Vector{vector.x * scaler, vector.y * scaler, vector.z * scaler};
   }
 
-  inline Vector operator/(const Vector &p, double s) {
-    return Vector{p.x / s, p.y / s, p.z / s};
+  inline Vector operator/(const Vector &vector, double scaler) {
+    return Vector{vector.x / scaler, vector.y / scaler, vector.z / scaler};
   }
 
-  inline Vector operator/(double s, const Vector &p) {
-    return Vector{p.x / s, p.y / s, p.z / s};
+  inline Vector operator/(double scaler, const Vector &vector) {
+    return Vector{vector.x / scaler, vector.y / scaler, vector.z / scaler};
   }
 
-  inline Vector operator+=(const Vector&p, const Vector &v) {
-    return Vector{p.x + v.x, p.y + v.y, p.z + v.z};
+  inline Vector operator+=(const Vector &v1, const Vector &v2) {
+    return Vector{v1.x + v2.x, v1.y + v2.y, v1.z + v2.z};
   }
 
-  inline Vector operator-=(const Vector&p, const Vector &v) {
-    return Vector{p.x - v.x, p.y - v.y, p.z - v.z};
+  inline Vector operator-=(const Vector &v1, const Vector &v2) {
+    return Vector{v1.x - v2.x, v1.y - v2.y, v1.z - v2.z};
   }
 
-  inline Vector unit_vector(Vector p) {
-    return p / p.length();
+  inline Vector unit_vector(Vector vector) {
+    return vector / vector.length();
   }
 }
 
